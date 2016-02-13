ed8519606203891048126095d7683acd8450d614
timkersch <tim.kersch@gmail.com>
2016-02-13 15:53:51 +0100

    Fixed twitter

--- backend/twitter_client.py
+++ backend/twitter_client.py
@@ -1,25 +1,22 @@
 from twitter import *
-import numpy as np
 from configs import twitter_conf
 
 if __name__ == "__main__":
 	conf = twitter_conf
 	twitter = Twitter(auth=OAuth(conf.a_key, conf.a_secret, conf.c_key, conf.c_secret))
 
-	def get_tweets(keyword, geocode, nmbr_of_tweets=100, lang="sv"):
-		res_arr = np.empty(nmbr_of_tweets)
+	def get_tweets(keyword, nmbr_of_tweets=100, lang="sv"):
+		res_arr = []
 		no_tweets = nmbr_of_tweets
 		prev = 0
-		for i in range(0, (nmbr_of_tweets % 100) + 1):
+		for i in range(0, int(nmbr_of_tweets / 100)):
 			if no_tweets <= 100:
-				query = twitter.search.tweets(q=keyword, geocode=geocode, count=no_tweets, lang=lang)
+				query = twitter.search.tweets(q=keyword, count=no_tweets, lang=lang)
 			else:
 				query = twitter.search.tweets(q=keyword, count=100, lang=lang)
 				no_tweets -= 100
 			for j in range(prev, len(query["statuses"])):
-				res_arr[j] = query["statuses"][j]["text"]
+				res_arr.append(query["statuses"][j]["text"])
 			prev += len(query["statuses"])
 
-		return len(res_arr)
-	
-	print(get_tweets('#usa', '57.68,12,200km', 1000))
+		return res_arr
