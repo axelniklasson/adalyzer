8e1f615ccb0e48dd03e244ea51915442d078fdaf
Axel Niklasson <axel.niklasson@live.com>
2016-02-13 11:01:51 +0100

    Changes

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -10,16 +10,42 @@ adalyzerControllers.controller('MapController', ['$scope', function($scope) {
 
 adalyzerControllers.controller('PersonasController', ['$scope', function($scope) {
 	$scope.title = 'Personas';
-	$scope.data = [
+	var data = [
 		{
 			'name': 'John Doe',
-			'age': '20 y/o'
+			'age': '20 y/o',
+			'interests': [
+				'Foo', 'Bar', 'Baz'
+			]
 		},
 		{
 			'name': 'Jane Doe',
-			'age': '20 y/o'
+			'age': '20 y/o',
+			'interests': [
+				'Foo', 'Bar', 'Baz'
+			]
+		},
+		{
+			'name': 'Steve Jobs',
+			'age': '20 y/o',
+			'interests': [
+				'Foo', 'Bar', 'Baz'
+			]
+		},
+		{
+			'name': 'Steve Wozniak',
+			'age': '20 y/o',
+			'interests': [
+				'Foo', 'Bar', 'Baz'
+			]
 		},
 	];
+
+	angular.forEach(data, function(value, key) {
+		value.img = 'img/persona-' + (key + 1) + '.png';
+	});
+	
+	$scope.personas = data;
 }]);
 
 adalyzerControllers.controller('StatsController', ['$scope', function($scope) {
