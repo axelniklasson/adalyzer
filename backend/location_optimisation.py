6a4f3459199a1edf714f69db1123456d69022916
timkersch <tim.kersch@gmail.com>
2016-02-13 13:28:09 +0100

    Updated location optimisation

--- backend/location_optimisation.py
+++ backend/location_optimisation.py
@@ -9,4 +9,13 @@ def find_optimal_locations(data, count = 5, no_clusters = 10):
 	for i in range(0, clusters.size):
 		classes_count[clusters[i], 0] = clusters[i]
 		classes_count[clusters[i], 1] += 1
+
 	sorted = classes_count[classes_count[:,1].argsort()]
+
+	cluster_locations = np.empty(count)
+	c = 0
+	for j in range(sorted[0].size, sorted[0].size - count, -1):
+		cluster_locations[c] = kmeans.cluster_centers_[sorted[j,0]]
+		c += 1
+
+	return cluster_locations
