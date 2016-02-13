6d37725497781f9b4ec894341fa8e8399567bef5
Johan Angséus <angseus@student.chalmers.se>
2016-02-13 13:11:29 +0100

    MappingController now works as intended

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,16 +7,63 @@ adalyzerControllers.controller('MainController', ['$scope', function($scope) {
 adalyzerControllers.controller('MapController', ['$scope', function($scope, NgMap) {
 	$scope.title = 'Key points';
 
-	$scope.$on('mapInitialized', function(event, map) {
-			$scope.map = map;
+	var images = [{
+		url: 'img/persona-map-1.png',
+		// This marker is 20 pixels wide by 32 pixels high.
+		size: new google.maps.Size(30, 33),
+		// The origin for this image is (0, 0).
+		origin: new google.maps.Point(0, 0),
+		// The anchor for this image is the base of the flagpole at (0, 32).
+		anchor: new google.maps.Point(0, 28)
+		},
+		{
+		url: 'img/persona-map-2.png',
+		// This marker is 20 pixels wide by 32 pixels high.
+		size: new google.maps.Size(30, 33),
+		// The origin for this image is (0, 0).
+		origin: new google.maps.Point(0, 0),
+		// The anchor for this image is the base of the flagpole at (0, 32).
+		anchor: new google.maps.Point(0, 28)
+		},
+		{url: 'img/persona-map-3.png',
+		// This marker is 20 pixels wide by 32 pixels high.
+		size: new google.maps.Size(30, 33),
+		// The origin for this image is (0, 0).
+		origin: new google.maps.Point(0, 0),
+		// The anchor for this image is the base of the flagpole at (0, 32).
+		anchor: new google.maps.Point(0, 28)
+		},
+		{url: 'img/persona-map-4.png',
+		// This marker is 20 pixels wide by 32 pixels high.
+		size: new google.maps.Size(30, 33),
+		// The origin for this image is (0, 0).
+		origin: new google.maps.Point(0, 0),
+		// The anchor for this image is the base of the flagpole at (0, 32).
+		anchor: new google.maps.Point(0, 28)
+		}];
 
-			var marker = new google.maps.Marker({
-    			title: "Hi marker "
-  			});
-  			var latlng = new google.maps.LatLng(57.68, 12);
-  			marker.setPosition(latlng);
-  			marker.setMap($scope.map);
+	$scope.$on('mapInitialized', function(event, map) {
+		$scope.map = map;
+		console.log("Map loaded. Ready to handle new markers");
+		add_markers(1, 57.68, 12);
+	});
+
+	var markers = [];
+
+	function add_markers(img_id, lat, lng){
+		var marker = new google.maps.Marker({
+			title: "car",
+			icon: images[img_id]
 		});
+
+		var latlng = new google.maps.LatLng(lat, lng);
+		marker.setPosition(latlng);
+		marker.setMap($scope.map);
+
+		markers.push(marker);			
+	};
+	
+
 }]);
 
 adalyzerControllers.controller('PersonasController', ['$scope', function($scope) {
