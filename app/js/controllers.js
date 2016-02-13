5482fc097f3db9865bd0a1ee806d3f54063fb294
Axel Niklasson <axel.niklasson@live.com>
2016-02-13 09:44:55 +0100

    Scaffold

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,5 +1,17 @@
 var adalyzerControllers = angular.module('AdalyzerControllers', ['AdalyzerServices']);
 
 adalyzerControllers.controller('MainController', ['$scope', function($scope) {
-	$scope.title = 'Hello World!';
+	// N/A
+}]);
+
+adalyzerControllers.controller('MapController', ['$scope', function($scope) {
+	$scope.title = 'Maps';
+}]);
+
+adalyzerControllers.controller('PersonasController', ['$scope', function($scope) {
+	$scope.title = 'Personas';
+}]);
+
+adalyzerControllers.controller('StatsController', ['$scope', function($scope) {
+	$scope.title = 'Stats';
 }]);
