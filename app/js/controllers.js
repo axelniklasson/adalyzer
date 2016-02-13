088c67342d0a7cbcac9a8f765306d4f62061389b
Johan Angséus <angseus@student.chalmers.se>
2016-02-13 13:45:11 +0100

    Two maps, one showing car locations and one showing personas

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,4 +1,4 @@
-var adalyzerControllers = angular.module('AdalyzerControllers', ['AdalyzerServices', 'AdalyzerDirectives']);
+var adalyzerControllers = angular.module('AdalyzerControllers', ['AdalyzerServices']);
 
 adalyzerControllers.controller('MainController', ['$scope', function($scope) {
 	// N/A
@@ -7,53 +7,58 @@ adalyzerControllers.controller('MainController', ['$scope', function($scope) {
 adalyzerControllers.controller('MapController', ['$scope', function($scope, NgMap) {
 	$scope.title = 'Key points';
 
-	var images = [{
-		url: 'img/persona-map-1.png',
+	// initialize an array with markers
+	var markers = [];
+
+	// car image
+	var image = {
+		url: 'img/car.png',
 		// This marker is 20 pixels wide by 32 pixels high.
-		size: new google.maps.Size(30, 33),
+		size: new google.maps.Size(50, 50),
 		// The origin for this image is (0, 0).
 		origin: new google.maps.Point(0, 0),
 		// The anchor for this image is the base of the flagpole at (0, 32).
-		anchor: new google.maps.Point(0, 28)
+		anchor: new google.maps.Point(0, 0)
+	};
+
+	// mockup car JSON
+	var cars = [
+		{
+			'id':'1',
+			'lat':'57.60',
+			'lng':'12'
 		},
 		{
-		url: 'img/persona-map-2.png',
-		// This marker is 20 pixels wide by 32 pixels high.
-		size: new google.maps.Size(30, 33),
-		// The origin for this image is (0, 0).
-		origin: new google.maps.Point(0, 0),
-		// The anchor for this image is the base of the flagpole at (0, 32).
-		anchor: new google.maps.Point(0, 28)
+			'id':'2',
+			'lat':'58.00',
+			'lng':'12.2'
 		},
-		{url: 'img/persona-map-3.png',
-		// This marker is 20 pixels wide by 32 pixels high.
-		size: new google.maps.Size(30, 33),
-		// The origin for this image is (0, 0).
-		origin: new google.maps.Point(0, 0),
-		// The anchor for this image is the base of the flagpole at (0, 32).
-		anchor: new google.maps.Point(0, 28)
-		},
-		{url: 'img/persona-map-4.png',
-		// This marker is 20 pixels wide by 32 pixels high.
-		size: new google.maps.Size(30, 33),
-		// The origin for this image is (0, 0).
-		origin: new google.maps.Point(0, 0),
-		// The anchor for this image is the base of the flagpole at (0, 32).
-		anchor: new google.maps.Point(0, 28)
-		}];
+		{
+			'id':'3',
+			'lat':'58.1',
+			'lng':'12.1'
+		}
+	];
 
+	// function that runs when the map has initialized
 	$scope.$on('mapInitialized', function(event, map) {
 		$scope.map = map;
 		console.log("Map loaded. Ready to handle new markers");
-		add_markers(1, 57.68, 12);
+		for(i = 0; i < cars.length; i++){
+			add_markers(cars[i].lat, cars[i].lng);	
+		}
+
+		// Move a marker to a new position
+		setTimeout(function() { var latlng = new google.maps.LatLng(57.68, 12); markers[0].setPosition(latlng); }, 5000);
+		
 	});
 
-	var markers = [];
-
-	function add_markers(img_id, lat, lng){
+	// function to add a new marker using img_id which is a persona id,
+	// lat and lng of the car / persona
+	function add_markers(lat, lng){
 		var marker = new google.maps.Marker({
 			title: "car",
-			icon: images[img_id]
+			icon: image
 		});
 
 		var latlng = new google.maps.LatLng(lat, lng);
@@ -61,11 +66,13 @@ adalyzerControllers.controller('MapController', ['$scope', function($scope, NgMa
 		marker.setMap($scope.map);
 
 		markers.push(marker);			
-	};
-	
+	}
+
+
 
 }]);
 
+/* Controllers fetching data for the different personas */
 adalyzerControllers.controller('PersonasController', ['$scope', function($scope) {
 	$scope.title = 'Personas';
 	var data = [
@@ -106,8 +113,71 @@ adalyzerControllers.controller('PersonasController', ['$scope', function($scope)
 	$scope.personas = data;
 }]);
 
+
+/* Controller for the Mapping id, which is a map of all personas and their locations */
 adalyzerControllers.controller('MappingController', ['$scope', function($scope) {
 	$scope.title = 'Mapping';
+
+	// array with images of personas
+	var images = [{
+		url: 'img/persona-map-1.png',
+		// This marker is 20 pixels wide by 32 pixels high.
+		size: new google.maps.Size(30, 33),
+		// The origin for this image is (0, 0).
+		origin: new google.maps.Point(0, 0),
+		// The anchor for this image is the base of the flagpole at (0, 32).
+		anchor: new google.maps.Point(0, 28)
+		},
+		{
+		url: 'img/persona-map-2.png',
+		// This marker is 20 pixels wide by 32 pixels high.
+		size: new google.maps.Size(30, 33),
+		// The origin for this image is (0, 0).
+		origin: new google.maps.Point(0, 0),
+		// The anchor for this image is the base of the flagpole at (0, 32).
+		anchor: new google.maps.Point(0, 28)
+		},
+		{url: 'img/persona-map-3.png',
+		// This marker is 20 pixels wide by 32 pixels high.
+		size: new google.maps.Size(30, 33),
+		// The origin for this image is (0, 0).
+		origin: new google.maps.Point(0, 0),
+		// The anchor for this image is the base of the flagpole at (0, 32).
+		anchor: new google.maps.Point(0, 28)
+		},
+		{url: 'img/persona-map-4.png',
+		// This marker is 20 pixels wide by 32 pixels high.
+		size: new google.maps.Size(30, 33),
+		// The origin for this image is (0, 0).
+		origin: new google.maps.Point(0, 0),
+		// The anchor for this image is the base of the flagpole at (0, 32).
+		anchor: new google.maps.Point(0, 28)
+		}];
+
+	// function that runs when the map has initialized
+	$scope.$on('mapInitialized', function(event, map) {
+		$scope.map = map;
+		console.log("Map loaded. Ready to handle new markers");
+		add_markers(1, 57.68, 12);
+	});
+
+	// initialize an array with markers
+	var markers = [];
+
+	// function to add a new marker using img_id which is a persona id,
+	// lat and lng of the car / persona
+	function add_markers(img_id, lat, lng){
+		var marker = new google.maps.Marker({
+			title: "car",
+			icon: images[img_id]
+		});
+
+		var latlng = new google.maps.LatLng(lat, lng);
+		marker.setPosition(latlng);
+		marker.setMap($scope.map);
+
+		markers.push(marker);			
+	}
 }]);
 
 adalyzerControllers.controller('StatsController', ['$scope', function($scope) {
